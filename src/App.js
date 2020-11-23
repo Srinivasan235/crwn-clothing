2854bdea23db7d43efd940a0d9e5aee648e8b173
Srinivasan-Ops <60819235+Srinivasan-Ops@users.noreply.github.com>
2020-11-23 19:27:36 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import HomePage from './pages/homepage/HomePage.component.jsx';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 function App() {
 	return (
 		<div className="container">
 			<BrowserRouter>
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route exact path="/shop" component={ShopPage} />
