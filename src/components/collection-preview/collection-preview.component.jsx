74dc73065e5881003e06a6e33730db4be69f5226
Srinivasan-Ops <60819235+Srinivasan-Ops@users.noreply.github.com>
2020-12-04 09:47:01 +0530

    updated collection page checkout page and refactored the code

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -17,5 +17,4 @@ const CollectionPreview = ({ title, items }) => (
         </div>
     </div>
 );
-
 export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,36 +1,16 @@
 import React from 'react';
-import SHOP_DATA from './shop.data.js'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
+import CollectionOverview from '../../components/collection-overview/collection-overview.component'
+import CollectionPage from '../collection/collection.component';
+import { Route } from 'react-router-dom';
+const ShopPage = ({ match }) => (
+    <div className="shop-page">
 
-class ShopPage extends React.Component {
-
-    constructor() {
-        super();
-
-        this.state = {
-            collections: SHOP_DATA
-        }
-    }
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
 
 
-    render() {
-        const { collections } = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.map(
-                        ({ id, ...otherCollectionProps }) => (
-                            <CollectionPreview key={id} {...otherCollectionProps} />
-                        )
-
-                    )
-                }
-            </div>
-
-        )
-    }
-
-}
+)
 
 
 
